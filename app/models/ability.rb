89d730718d4192477fa94c371086e2356c2fdc7d
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-11 16:50:51 -0300

    feature: add authorizations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,9 @@ class Ability
 
   def initialize(user)
     return unless user.present? # additional permissions for logged in users
-    can :manage, :all
+    can :manage, User, id: user.id
+    can :manage, Group, user_id: user.id
+    can :manage, UserTransaction, author_id: user.id
+    can :manage, TransactionGroup, group_id: { user_id: user.id }
   end
 end
