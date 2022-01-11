8fa7853181de6e02c5ce21d3837223dde1364123
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-11 19:09:12 -0300

    feature: add protected method to provid the nav link_options

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,21 @@
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    @welcome = true
+    navbar_options
+  end
+
+  protected 
+
+  def navbar_options
+    @links = {
+      login: {
+        name: 'Log in',
+        path: new_user_session_path
+      },
+      sign_up:  {
+        name: 'Sign up',
+        path: new_user_registration_path
+      }
+    }
+  end
 end
