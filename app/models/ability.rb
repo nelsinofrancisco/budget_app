79baba673adc55d4d8345e47b657614e92e50633
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-11 16:58:20 -0300

    feature: fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,10 +1,9 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     return unless user.present? # additional permissions for logged in users
+
     can :manage, User, id: user.id
     can :manage, Group, user_id: user.id
     can :manage, UserTransaction, author_id: user.id
