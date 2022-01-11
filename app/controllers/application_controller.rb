087c5f80da2ef241d4d7c8af580f2c80c56f92cb
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-11 16:57:56 -0300

    feature: add @user = current_user to before_action callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :logged_user
+
+  protected
+
+  def logged_user
+    @user = current_user
+  end
 end
