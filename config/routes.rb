5d7b5f57a49d31cc286f004eaa81ae9a8448d629
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-11 16:57:35 -0300

    feature: fix routes for authenticated and unauthenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'welcome#index', as: :unauthenticated_root
+    end
+  end
 
   root to: 'welcome#index'
 end
