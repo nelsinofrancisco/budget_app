5cf044ca4700d2469a34b16c1da9147731d3397b
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-11 16:29:35 -0300

    feature: add most_recent transaction groups for Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,8 @@ class Group < ApplicationRecord
   validates :name, :icon, presence: true
   belongs_to :user
   has_many :transaction_groups, dependent: :destroy
+
+  def most_recent
+    self.transaction_groups.where(group_id: self.id).order(created_at: :desc).limit(5)
+  end
 end
