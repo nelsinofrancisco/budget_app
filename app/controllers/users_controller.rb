3901409880c3f6760f815860433e028e9b688f2b
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-12 17:57:49 -0300

    feature: implement nav_bar view for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  def navbar
+    render_nav(true)
+    super
+  end
+
+  protected
+
+  def navbar_options
+    @links = {
+      login: {
+        name: 'Log in',
+        path: new_user_session_path
+      },
+      sign_up: {
+        name: 'Sign up',
+        path: new_user_registration_path
+      }
+    }
+  end
+
+  def render_nav(boolean)
+    @regular_n = boolean if boolean
+  end
 end
