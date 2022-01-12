77e30b14df14cad9396559cc39a058a5d1e0a51a
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-12 17:23:58 -0300

    feature: change permitted_params to sign_up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -27,13 +27,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys:
-      %i[name email password password_confirmation posts_counter role])
+      %i[name email password password_confirmation])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update,
-                                      keys: %i[name bio email password password_confirmation role])
+                                      keys: %i[name email password password_confirmation])
   end
 
   def navbar_options
