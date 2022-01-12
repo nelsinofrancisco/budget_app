edf5b53fc938e52262f96f84640747a6e5cfcd9c
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-12 16:30:17 -0300

    feature: reduce code by changing the navbar action to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :logged_user
 
+  def navbar
+    navbar_options
+    respond_to do |format|
+      format.js { render 'navigations/navbar.js.erb' }
+    end
+  end
+
   protected
 
   def logged_user
