1e2bf94bbf9cfef8a49cc18ef5670987f918daf5
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-14 20:05:29 -0300

    feature: add new action to user_transactions

--- app/controllers/user_transactions_controller.rb
+++ app/controllers/user_transactions_controller.rb
@@ -1,5 +1,11 @@
 class UserTransactionsController < ApplicationController
-  def new; end
+  def new
+    render_nav(true)
+    respond_to do |format|
+      format.js { render 'user_transactions/js/new.js.erb' }
+      format.html
+    end
+  end
 
   def create; end
 end
