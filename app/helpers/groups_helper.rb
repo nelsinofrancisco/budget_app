bd5b1271ed085123a697bc0d70d14ab7bd53b4c7
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-14 13:00:28 -0300

    feature: move method to calculate total spent to users_helper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,9 +1,2 @@
 module GroupsHelper
-  def transactions_total
-    transaction_total = 0
-    @user_transactions.each do |transaction|
-      transaction_total += transaction.amount
-    end
-    transaction_total
-  end
 end
