2e18104b26290a40e2b7b0d0f8f6bf981fd538fd
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-14 12:49:13 -0300

    feature: add method to count the total amount of transactions

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,9 @@
 module GroupsHelper
+  def transactions_total
+    transaction_total = 0
+    @user_transactions.each do |transaction|
+      transaction_total += transaction.amount
+    end
+    transaction_total
+  end
 end
