057d74fb5e04212156134695420ec376f9c703eb
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-01-14 20:56:53 -0300

    feature: add create action to user_transactions

--- app/controllers/user_transactions_controller.rb
+++ app/controllers/user_transactions_controller.rb
@@ -7,5 +7,22 @@ class UserTransactionsController < ApplicationController
     end
   end
 
-  def create; end
+  def create
+    @user_transaction = UserTransaction.new(transaction_params)
+
+    if @user_transaction.save
+      TransactionGroup.create(transaction_group_params.merge(user_transaction_id: @user_transaction.id))
+      redirect_to groups_path, format: 'js' 
+    end
+  end
+
+  protected
+
+  def transaction_params
+    params.require(:user_transaction).permit(:name, :amount, :author_id)
+  end
+
+  def transaction_group_params
+    params.require(:user_transaction).permit(:group_id)
+  end
 end
